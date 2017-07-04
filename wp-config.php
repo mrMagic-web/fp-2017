701d6bcff81cdf44243827d424402c67a082e805
Maciej Drahusz <maciejdrahusz@gmail.com>
2017-07-04 14:16:41 +0200

    Installed wordpress

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@ if (file_exists(dirname(__FILE__) . '/local-config.php')) {
 
 // Global DB config
 if (!defined('DB_NAME')) {
-	define('DB_NAME', 'yeopress');
+	define('DB_NAME', 'fastpack');
 }
 if (!defined('DB_USER')) {
 	define('DB_USER', 'root');
@@ -52,14 +52,14 @@ if (!defined('DB_COLLATE')) {
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'j,6otV$~rDz*H#vF,5 8]1N$-/M?+l{sa_T~ZMm:3 AGU2*?N|5La4)Zus?zY*HO');
-define('SECURE_AUTH_KEY',  'lLTh-RqF?KL}|P-1PZo57MsJTXtqY3qHM!kz6c@XO*v_uN7V`lA!9u(rA=z|+0fS');
-define('LOGGED_IN_KEY',    'PZW1?cC1qp`=kG|chHY:.B.&CJik/<_ u$#RVRam`[-Iv@I|ep):|}=JoYmGW`v ');
-define('NONCE_KEY',        '0o k&%l6ev 8!.=(pJ_&@{l&oJ#q ~-$+Mv-PW`x]i64TA&c9G}VV|Lnh2}$+Km}');
-define('AUTH_SALT',        'U$Jv-sV9wq[P[|^>)U*-5OrnJ!}dr.J=kP0-a6QGQ|k/Y-4PNcZ[GBmXXurFEn|,');
-define('SECURE_AUTH_SALT', 'f-A!_i]pmc.g.H=/lq(H80tx1}.rR@+754vR~+KmPNs1Cr|Ggv8I|ZEY_Et ;>^k');
-define('LOGGED_IN_SALT',   'hG<^8%dKynZ}b9Rvf;+I?;IB8ff&L_lr;yN|UG_d/{8D`sNaT_(qcnH)*|^4M3nD');
-define('NONCE_SALT',       ':@n%PU8P^-VzQ#aO#A+qYX$O(Zn0b@Xq0|%O:x|*`.bjIamS:t+:Tb@;11w}<Ez[');
+define('AUTH_KEY',         'lM-{</6cL[o:7HfG7|NJ{gZm!gHN;S(_fon-omRmbc.Rm [y3v |]>x!VZBSNx<,');
+define('SECURE_AUTH_KEY',  '^/~>Ah9+|4>LbNw,j3hQT4ZzZ}o=eTt/a|mqN|L!QB2T+P*$W|IiO+@3O/eseN$n');
+define('LOGGED_IN_KEY',    'b>QQ#f1Rz!_xT~(Rd`M+!4Ay<L~ Tr}0fTB<cL-wtU^jQgRV,8`B5 p08)(c_0I.');
+define('NONCE_KEY',        'gA,D-_.d-o)5~C<`EJTHR?0}67|;BP[VtX$b S.kcwSmegg`>=,x+Cs97+&p|5Q{');
+define('AUTH_SALT',        '.5*s:Qg6x(U0u8ox+u.F)J_|w8J>$3:t{q+I<n7`OYppdsD96~!>XCYn2<(t8np)');
+define('SECURE_AUTH_SALT', '?|3HY5xaJFwbZA.[4(GCLA5TQ<^<IMK>L-q=>ip=oUq9j-Mq?9ti;xao%zmW<it/');
+define('LOGGED_IN_SALT',   'v_9qZQLJuREf#=jfsC+l; e=1{=<t|b]_>{=~451ds}b5lWzfeC+ma>iF75}]$tW');
+define('NONCE_SALT',       ';NcK)T~.etqgU!Sa[,o(N&GoYCP(}{u^IQu?VUXJ7w+jCLB4KUhg,(P-*K>68rCv');
 
 /**#@-*/
 
@@ -69,7 +69,7 @@ define('NONCE_SALT',       ':@n%PU8P^-VzQ#aO#A+qYX$O(Zn0b@Xq0|%O:x|*`.bjIamS:t+:
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'fp_';
+$table_prefix  = 'wp_';
 
 /**
  * WordPress Localized Language, defaults to English.
@@ -81,24 +81,6 @@ $table_prefix  = 'fp_';
  */
 define('WPLANG', '');
 
-
-/**
- * Set custom paths
- *
- * These are required because wordpress is installed in a subdirectory.
- */
-if (!defined('WP_SITEURL')) {
-	define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . ':1234/cms');
-}
-if (!defined('WP_HOME')) {
-	define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME'] . ':1234');
-}
-if (!defined('WP_CONTENT_DIR')) {
-	define('WP_CONTENT_DIR', dirname(__FILE__) . '/content');
-}
-if (!defined('WP_CONTENT_URL')) {
-	define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . ':1234/content');
-}
 
 
 /**
